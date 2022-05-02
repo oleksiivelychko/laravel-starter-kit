635da4e2682b13bbafca4414a7ae28fd4e9c9ba4
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-02 13:26:07 +0300

    load custom project settings

---
+++ config/settings.php
@@ -0,0 +1,30 @@
+<?php
+
+return [
+    'schema' => [
+        'string_length' => 191,
+        'pagination_limit' => 10
+    ],
+
+    'assets' => [
+        'not-found' => 'images/not-found.jpg'
+    ],
+
+    'import' => [
+        'products_json' => '/app/storage/app/public/import/products.json'
+    ],
+
+    'languages' => [
+        'English'       => 'en',
+        'Українська'    => 'uk',
+    ],
+
+    'intl' => [
+        'en' => [
+            'en_US' => 'America/New_York',
+        ],
+        'uk' => [
+            'uk_UA' => 'Europe/Kiev',
+        ],
+    ]
+];

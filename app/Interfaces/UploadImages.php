aac9a74b52f3ce0eaecbc3189dc8d08fc6d0f237
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-02 17:09:29 +0300

    define common image uploading methods

---
+++ app/Interfaces/UploadImages.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Interfaces;
+
+use Illuminate\Http\UploadedFile;
+
+
+interface UploadImages
+{
+    public function getImageField(): string;
+
+    public function getImagesFolder(): string;
+
+    public function getCropPresets(): array;
+
+    public function uploadImage(?UploadedFile $uploadedFile, $multiple=false): bool|string;
+
+    public function uploadImages(array $data): void;
+}

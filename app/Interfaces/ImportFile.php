8c194386796042f978723a728e9ff58c2862ccd2
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-02 17:09:44 +0300

    define import file

---
+++ app/Interfaces/ImportFile.php
@@ -0,0 +1,11 @@
+<?php
+
+namespace App\Interfaces;
+
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+
+interface ImportFile
+{
+    public static function import(string $classname, UploadedFile $file): array;
+}

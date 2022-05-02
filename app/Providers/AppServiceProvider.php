558c5c92993d64991b53f0b2faaa045a8f51956a
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-02 16:59:55 +0300

    load configuration

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -2,7 +2,10 @@
 
 namespace App\Providers;
 
+use Illuminate\Http\Resources\Json\JsonResource;
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Support\Facades\Schema;
+
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -18,11 +21,10 @@ class AppServiceProvider extends ServiceProvider
 
     /**
      * Bootstrap any application services.
-     *
-     * @return void
      */
-    public function boot()
+    public function boot(): void
     {
-        //
+        Schema::defaultStringLength(config('settings.schema.string_length', 191));
+        JsonResource::withoutWrapping();
     }
 }

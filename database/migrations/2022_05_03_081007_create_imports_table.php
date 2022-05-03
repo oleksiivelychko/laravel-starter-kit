243a2a6aa0515f9f8a23ae47394baae97823f9a7
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-03 08:11:41 +0300

    create job import table

---
+++ database/migrations/2022_05_03_081007_create_imports_table.php
@@ -0,0 +1,28 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use App\Models\Order;
+
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('imports', function (Blueprint $table) {
+            $table->id();
+            $table->string('entity');
+            $table->enum('state', Import::STATES)->default(Import::STATE_INIT);
+            $table->unsignedInteger('received')->default(0);
+            $table->unsignedInteger('created')->default(0);
+            $table->unsignedInteger('updated')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('imports');
+    }
+};

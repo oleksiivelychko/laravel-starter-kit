0a48b1b8935a19c61031afa918710d836f8032dc
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-03 08:38:44 +0300

    create verify email notification

---
+++ app/Notifications/VerifyEmail.php
@@ -0,0 +1,43 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Auth\Notifications\VerifyEmail as Notification;
+use Illuminate\Support\Facades\URL;
+use Illuminate\Support\Carbon;
+
+
+class VerifyEmail extends Notification
+{
+    /**
+     * Get the notification's delivery channels.
+     */
+    public function via(mixed $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(mixed $notifiable): MailMessage
+    {
+        return (new MailMessage)
+            ->subject(__('auth.verify-email'))
+            ->view('emails.verify-email', ['url' => $this->verificationUrl($notifiable)]);
+    }
+
+    protected function verificationUrl($notifiable): string
+    {
+        return URL::temporarySignedRoute(
+            'verification.verify',
+            Carbon::now()->addMinutes(config('auth.verification.expire', 60)),
+            [
+                'locale' => app()->getLocale(),
+                'id' => $notifiable->getKey(),
+                'hash' => sha1($notifiable->getEmailForVerification())
+            ]
+        );
+    }
+}

a6959ee48ac253c42264539eead0aa841a0504d6
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-03 08:29:44 +0300

    create reset password notification

---
+++ app/Notifications/ResetPassword.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Notifications;
+
+use Illuminate\Notifications\Messages\MailMessage;
+use Illuminate\Auth\Notifications\ResetPassword as Notification;
+
+
+class ResetPassword extends Notification
+{
+    /**
+     * Get the notification's delivery channels.
+     */
+    public function via(mixed $notifiable): array
+    {
+        return ['mail'];
+    }
+
+    /**
+     * Get the mail representation of the notification.
+     */
+    public function toMail(mixed $notifiable): MailMessage
+    {
+        $url = url(route('password.reset', [
+            'locale'    => app()->getLocale(),
+            'token'     => $this->token,
+            'email'     => $notifiable->getEmailForPasswordReset(),
+        ], false));
+
+        return (new MailMessage)
+            ->subject(__('auth.reset-password'))
+            ->view('emails.reset-password', [
+                'url'   => $url,
+                'count' => config('auth.passwords.'.config('auth.defaults.passwords').'.expire')
+            ]);
+    }
+}

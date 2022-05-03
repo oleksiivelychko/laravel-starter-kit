dcdd7a30b8a61ccb6c0daad819fb015a74559d06
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-03 08:42:29 +0300

    extend user model

--- app/Models/User.php
+++ app/Models/User.php
@@ -2,14 +2,28 @@
 
 namespace App\Models;
 
+use App\Interfaces\Pagination;
+use App\Interfaces\UploadImages;
+use App\Traits\ACL;
+use App\Traits\Asset;
+use App\Notifications\ResetPassword;
+use App\Notifications\VerifyEmail;
 use Illuminate\Contracts\Auth\MustVerifyEmail;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Foundation\Auth\User as Authenticatable;
+use Illuminate\Http\UploadedFile;
 use Illuminate\Notifications\Notifiable;
+use Illuminate\Http\Request;
+use Illuminate\Contracts\Pagination\LengthAwarePaginator;
+use Illuminate\Support\Facades\Hash;
 
-class User extends Authenticatable
+
+/**
+ * @method select(string[] $array)
+ */
+class User extends Authenticatable implements MustVerifyEmail, UploadImages, Pagination
 {
-    use HasFactory, Notifiable;
+    use HasFactory, Notifiable, ACL, Asset;
 
     /**
      * The attributes that are mass assignable.
@@ -20,6 +34,7 @@ class User extends Authenticatable
         'name',
         'email',
         'password',
+        'avatar',
     ];
 
     /**
@@ -40,4 +55,85 @@ class User extends Authenticatable
     protected $casts = [
         'email_verified_at' => 'datetime',
     ];
+
+    protected string $imageField = 'avatar';
+
+    protected string $imagesFolder = 'avatars';
+
+    protected array $cropPresets = [[150,150]];
+
+    public function getImageField(): string
+    {
+        return $this->imageField;
+    }
+
+    public function getImagesFolder(): string
+    {
+        return $this->imagesFolder;
+    }
+
+    public function getCropPresets(): array
+    {
+        return $this->cropPresets;
+    }
+
+    public function setPasswordAttribute(string $value)
+    {
+        $this->attributes['password'] = Hash::make($value);
+    }
+
+    public function sendPasswordResetNotification($token)
+    {
+        $this->notify(new ResetPassword($token));
+    }
+
+    public function getUsernameAttribute(): string
+    {
+        return $this->attributes['name'] ?: $this->attributes['email'];
+    }
+
+    public function sendEmailVerificationNotification()
+    {
+        $this->notify(new VerifyEmail);
+    }
+
+
+    public function store($rolesIds=[], $permissionsIds=[], $uploadedAvatar=null): bool
+    {
+        $saved = $this->save();
+        if ($saved) {
+            $this->assignRoles($rolesIds);
+            $this->assignPermissions($permissionsIds);
+
+            if ($uploadedAvatar instanceof UploadedFile) {
+                $filename = $this->uploadImage($uploadedAvatar);
+                if ($filename) {
+                    $this->{$this->getImageField()} = $filename;
+                    $this->save();
+                }
+            }
+        }
+
+        return $saved;
+    }
+
+    public function pagination(Request $request, ?string $locale=null): LengthAwarePaginator
+    {
+        $sortColumn = $request->query('sort', 'id');
+        $sortDirection = $request->query('direction', 'asc');
+
+        return $this->select(['id', 'name', 'email', 'created_at'])
+            ->orderBy($sortColumn, $sortDirection)
+            ->paginate($this->getPaginationLimit());
+    }
+
+    public function getPaginationLimit(): int
+    {
+        return config('settings.schema.pagination_limit', 10);
+    }
+
+    public function uploadImages(array $data): void
+    {
+        // TODO: Implement uploadImages() method.
+    }
 }

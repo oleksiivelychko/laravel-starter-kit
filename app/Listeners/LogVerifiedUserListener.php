27aea339cb95e99918622205fdea5dc7ee0e3c9f
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-03 08:41:02 +0300

    create verified user listener

---
+++ app/Listeners/LogVerifiedUserListener.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace App\Listeners;
+
+use Illuminate\Support\Facades\Log;
+
+class LogVerifiedUserListener
+{
+    /**
+     * Create the event listener.
+     */
+    public function __construct()
+    {
+        //
+    }
+
+    /**
+     * Handle the event.
+     */
+    public function handle($event): void
+    {
+        Log::channel('daily')->info(
+            'User has verified email: '.$event->user->getEmailForVerification()
+        );
+    }
+}

0c23a70742bd345c6cee4a71de85eef5f4169e9c
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-04 10:14:49 +0300

    create locale controller

---
+++ app/Http/Controllers/LocaleController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Contracts\Foundation\Application;
+use Illuminate\Http\RedirectResponse;
+use Illuminate\Routing\Redirector;
+
+
+class LocaleController extends Controller
+{
+    public function change(string $locale): Redirector|Application|RedirectResponse
+    {
+        if (!$locale || !in_array($locale, array_values(config('settings.languages')))) {
+            $locale = config('app.fallback_locale');
+        }
+
+        app()->setLocale($locale);
+
+        $prevRoute = app('router')->getRoutes()->match(app('request')->create(url()->previous()));
+        if ($prevRoute) {
+            $prevRoute->setParameter('locale', $locale);
+            return redirect(route($prevRoute->getName(), $prevRoute->parameters()));
+        }
+
+        return redirect(route('home', $locale));
+    }
+}

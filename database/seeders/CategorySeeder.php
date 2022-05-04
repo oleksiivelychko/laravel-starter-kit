0c60263fdfe758531e6e3d59201ce452014a23e4
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-04 09:10:20 +0300

    create category seeder

---
+++ database/seeders/CategorySeeder.php
@@ -0,0 +1,33 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\Category;
+
+
+class CategorySeeder extends SchemaSeeder
+{
+    public function __construct()
+    {
+        parent::__construct(Category::class);
+    }
+
+    public function run(): void
+    {
+        $category = new Category;
+        $category->name = json_encode(['en' => 'Drinks', 'uk' => 'Напої'], JSON_UNESCAPED_UNICODE);
+        $category->slug = 'drinks';
+        $category->save();
+
+        $parentCategory = new Category;
+        $parentCategory->name = json_encode(['en' => 'Lunches', 'uk' => 'Обід'], JSON_UNESCAPED_UNICODE);
+        $parentCategory->slug = 'lunches';
+        $parentCategory->save();
+
+        $category = new Category;
+        $category->name = json_encode(['en' => 'Pizzas', 'uk' => 'Піци'], JSON_UNESCAPED_UNICODE);
+        $category->slug = 'pizzas';
+        $category->parent_id = $parentCategory->id;
+        $category->save();
+    }
+}

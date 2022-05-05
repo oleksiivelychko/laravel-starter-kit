b587b2033ad719e2938f8f08b44e422f49b354ce
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-05 11:58:14 +0300

    add translations

---
+++ lang/en/email.php
@@ -0,0 +1,17 @@
+<?php
+
+return [
+    'all-rights-reserved' => 'All rights reserved.',
+    'hello' => 'Hello!',
+    'regards' => 'Regards',
+    'copy-and-paste-url' => 'If you’re having trouble clicking the ":button" button, copy and paste the URL below into your web browser',
+    'reset-password' => [
+        'title_text' => 'You are receiving this email because we received a password reset request for your account.',
+        'reset-link-expire' => 'This password reset link will expire in :count minutes.',
+        'no-further-action-required' => 'If you did not request a password reset, no further action is required.'
+    ],
+    'verify-email' => [
+        'title_text' => 'Please click the button below to verify your email address.',
+        'no-further-action-required' => 'If you did not create an account, no further action is required.'
+    ]
+];
--- lang/en/validation.php
+++ lang/en/validation.php
@@ -136,6 +136,8 @@ return [
     'url' => 'The :attribute must be a valid URL.',
     'uuid' => 'The :attribute must be a valid UUID.',
 
+    'category-required' => 'A category is required.',
+
     /*
     |--------------------------------------------------------------------------
     | Custom Validation Language Lines

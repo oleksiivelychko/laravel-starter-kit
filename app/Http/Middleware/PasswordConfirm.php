a0abce26d8071e2a7cc8fc25d61979733561adad
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-05 10:38:02 +0300

    override RequirePassword middleware

---
+++ app/Http/Middleware/PasswordConfirm.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Http\Middleware;
+
+use Closure;
+use Illuminate\Auth\Middleware\RequirePassword;
+
+
+class PasswordConfirm extends RequirePassword
+{
+    public function handle($request, Closure $next, $redirectToRoute = null, $passwordTimeoutSeconds = null)
+    {
+        if ($this->shouldConfirmPassword($request, $passwordTimeoutSeconds)) {
+            if ($request->expectsJson()) {
+                return $this->responseFactory->json([
+                    'message' => trans('auth.password-required'),
+                ], 423);
+            }
+
+            return $this->responseFactory->redirectGuest(
+                $this->urlGenerator->route($redirectToRoute ?: 'password.confirm', app()->getLocale())
+            );
+        }
+
+        return $next($request);
+    }
+}

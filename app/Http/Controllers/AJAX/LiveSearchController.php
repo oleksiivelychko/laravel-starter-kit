5b5fa5b546ab94824fa1d77562006614119a74e9
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-06 08:17:57 +0300

    create ajax search controllers

---
+++ app/Http/Controllers/AJAX/LiveSearchController.php
@@ -0,0 +1,48 @@
+<?php
+
+namespace App\Http\Controllers\AJAX;
+
+use App\Http\Controllers\Controller;
+use App\Models\Category;
+use App\Models\Product;
+use App\Models\User;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+
+
+class LiveSearchController extends Controller
+{
+    protected array $allowedEntities = [
+        'user'      => User::class,
+        'category'  => Category::class,
+        'product'   => Product::class,
+    ];
+
+    public function search(Request $request, string $locale, $entity): JsonResource
+    {
+        if (!in_array($entity, array_keys($this->allowedEntities))) {
+            return new JsonResource([]);
+        }
+
+        $search = $request->post('search', '');
+
+        return match ($entity) {
+            'user' => new JsonResource(
+                $this->allowedEntities[$entity]
+                    ::select(['id as value', 'email as text'])
+                    ->where('name', 'LIKE', $search . '%')
+                    ->orWhere('name', 'LIKE', '%' . $search . '%')
+                    ->orWhere('name', 'LIKE', '%' . $search)
+                    ->get()->toArray()
+            ),
+            default => new JsonResource(
+                $this->allowedEntities[$entity]
+                    ::select(['id as value', "name->$locale as text"])
+                    ->where("name->$locale", 'LIKE', $search . '%')
+                    ->orWhere("name->$locale", 'LIKE', '%' . $search . '%')
+                    ->orWhere("name->$locale", 'LIKE', '%' . $search)
+                    ->get()->toArray()
+            ),
+        };
+    }
+}
---
+++ app/Http/Controllers/AJAX/SearchController.php
@@ -0,0 +1,53 @@
+<?php
+
+namespace App\Http\Controllers\AJAX;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Illuminate\Http\Resources\Json\JsonResource;
+use Illuminate\Support\Facades\DB;
+
+
+class SearchController extends Controller
+{
+    protected array $searchEntities;
+
+    public function __construct()
+    {
+        parent::__construct();
+
+        /**
+         * Table name => Route name
+         */
+        $this->searchEntities = [
+            'categories' => 'category.edit',
+            'products' => 'product.edit'
+        ];
+    }
+
+    public function search(Request $request, string $locale): JsonResource
+    {
+        $querySearch = $request->get('querySearch');
+        if (!$querySearch) {
+            return new JsonResource([]);
+        }
+
+        $queryBuilder = null;
+        foreach ($this->searchEntities as $table=>$route) {
+            $query = DB::table($table)
+                ->select(['id', "name->$locale as text", DB::raw("'$route' as route_name")])
+                ->where("name->$locale", 'LIKE', $querySearch . '%')
+                ->orWhere("name->$locale", 'LIKE', '%' . $querySearch . '%')
+                ->orWhere("name->$locale", 'LIKE', '%' . $querySearch);
+
+            $queryBuilder = $queryBuilder ? $queryBuilder->union($query) : $query;
+        }
+
+        $links = [];
+        foreach ($queryBuilder->get() as $row) {
+            $links[$row->text] = route($row->route_name, ['locale' => $locale, $row->id]);
+        }
+
+        return new JsonResource($links);
+    }
+}

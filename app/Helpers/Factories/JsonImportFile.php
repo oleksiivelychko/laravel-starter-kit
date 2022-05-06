b3d7587e00eccc31b0953094e9af293d3dedc38a
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-06 11:07:35 +0300

    create import factory methods

---
+++ app/Helpers/Factories/JsonImportFile.php
@@ -0,0 +1,17 @@
+<?php
+
+namespace App\Helpers\Factories;
+
+use App\Helpers\JsonImport;
+use App\Interfaces\ImportFile;
+use Symfony\Component\HttpFoundation\File\UploadedFile;
+
+
+class JsonImportFile implements ImportFile
+{
+    public static function import(string $classname, UploadedFile $file): array
+    {
+        $jsonDecode = json_decode(file_get_contents($file), true);
+        return JsonImport::import($classname, $jsonDecode);
+    }
+}

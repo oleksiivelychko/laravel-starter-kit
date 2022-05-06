12bd89fbc9c61054ffc1d9db8fb9a58ce6b32c60
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-06 09:05:18 +0300

    generate API token command

---
+++ app/Console/Commands/GenerateTokenCommand.php
@@ -0,0 +1,46 @@
+<?php
+
+namespace App\Console\Commands;
+
+use Exception;
+use Illuminate\Console\Command;
+
+
+class GenerateTokenCommand extends Command
+{
+    private const RANDOM_BYTES_LENGTH = 16;
+
+    /**
+     * The name and signature of the console command.
+     *
+     * @var string
+     */
+    protected $signature = 'generate:token';
+
+    /**
+     * The console command description.
+     *
+     * @var string
+     */
+    protected $description = 'Generate token for API authorization';
+
+    /**
+     * Create a new command instance.
+     *
+     * @return void
+     */
+    public function __construct()
+    {
+        parent::__construct();
+    }
+
+    /**
+     * Execute the console command.
+     * @throws Exception
+     */
+    public function handle()
+    {
+        $token = bin2hex(random_bytes(self::RANDOM_BYTES_LENGTH));
+        echo 'Token has been successfully generated: '.$token.PHP_EOL;
+    }
+}

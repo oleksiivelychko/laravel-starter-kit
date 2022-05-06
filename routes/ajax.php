612b8309e11f2fdcd773ba31897de2b3cf53cc6b
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-06 11:24:42 +0300

    create ajax routes

---
+++ routes/ajax.php
@@ -0,0 +1,17 @@
+<?php
+
+use App\Http\Controllers\Ajax\LiveSearchController;
+use App\Http\Controllers\Ajax\SearchController;
+use Illuminate\Support\Facades\Route;
+
+
+Route::group([
+    'prefix' => '{locale}',
+    'middleware' => 'locale',
+    'where' => ['locale' => rtrim(implode('|', array_values(config('settings.languages'))), '|')]
+], function () {
+    Route::group(['prefix' => 'ajax'], function () {
+        Route::post('live-search-{entity}', [LiveSearchController::class, 'search']);
+        Route::post('search', [SearchController::class, 'search'])->name('search');
+    });
+});

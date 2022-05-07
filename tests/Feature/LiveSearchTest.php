455eaa9fbde202ef3b9c5af4ecb1fab442d032d6
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-07 10:26:57 +0300

    create live search tests

---
+++ tests/Feature/LiveSearchTest.php
@@ -0,0 +1,57 @@
+<?php
+
+namespace Tests\Feature;
+
+use App\Models\Category;
+use App\Models\Product;
+use App\Traits\Translation;
+use Illuminate\Foundation\Testing\RefreshDatabase;
+use Tests\TestCase;
+
+
+class LiveSearchTest extends TestCase
+{
+    use RefreshDatabase;
+
+    public function test_live_search_categories()
+    {
+        /** @var Category $category */
+        $category = Category::factory()->create();
+
+        $response = $this->json('POST', '/'.app()->getLocale().'/ajax/live-search-category', [
+            'search' => Translation::translateObject($category->name)
+        ], [
+            'HTTP_X-Requested-With' => 'XMLHttpRequest'
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([['value', 'text']])
+            ->assertExactJson(json_decode($response->content(), true));
+
+        $content = json_decode($response->content(), true)[0];
+        $this->assertEquals($category->id, $content['value']);
+        $this->assertEquals(Translation::translateObject($category->name), $content['text']);
+    }
+
+    public function test_live_search_products()
+    {
+        /** @var Product $product */
+        $product = Product::factory()->create();
+
+        $response = $this->json('POST', '/'.app()->getLocale().'/ajax/live-search-product', [
+            'search' => Translation::translateObject($product->name)
+        ], [
+            'HTTP_X-Requested-With' => 'XMLHttpRequest'
+        ]);
+
+        $response
+            ->assertStatus(200)
+            ->assertJsonStructure([['value', 'text']])
+            ->assertExactJson(json_decode($response->content(), true));
+
+        $content = json_decode($response->content(), true)[0];
+        $this->assertEquals($product->id, $content['value']);
+        $this->assertEquals(Translation::translateObject($product->name), $content['text']);
+    }
+}

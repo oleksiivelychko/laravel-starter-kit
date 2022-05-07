e7694ba41f551bfcb8136f9624af65b93f2fcf1d
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-07 09:40:53 +0300

    define dashboard scripts

---
+++ resources/js/dashboard.js
@@ -0,0 +1,7 @@
+import './modules/echo';
+import './modules/alert';
+import './modules/live-search';
+import './modules/wysiwyg';
+import './modules/selectable';
+import './modules/search';
+import {Collapse} from "bootstrap";

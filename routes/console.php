1135e8e963106970242696433abed56cc891c5a2
Oleksii Velychko <oleksiivelychko@icloud.com>
2022-05-07 10:31:23 +0300

    create test email send console command

--- routes/console.php
+++ routes/console.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Foundation\Inspiring;
 use Illuminate\Support\Facades\Artisan;
+use Illuminate\Support\Facades\Mail;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,6 +14,9 @@ use Illuminate\Support\Facades\Artisan;
 |
 */
 
-Artisan::command('inspire', function () {
-    $this->comment(Inspiring::quote());
-})->purpose('Display an inspiring quote');
+Artisan::command('test-mail', function () {
+    Mail::send('components/application-version', [], function ($message) {
+        $message->to('test@test.test')->subject('test mail');
+    });
+    $this->comment('check now https://mail.laravel-dashboard.local');
+})->purpose('Test sending an email to MailHog server');

e20dbbd5bb87b31ffd387333ada89177a21f71bb
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-01-21 13:29:58 +0200

    make changes according to 2 version of library

--- resources/js/modules/live-search.js
+++ resources/js/modules/live-search.js
@@ -1,4 +1,4 @@
-import SlimSelect from 'slim-select'
+import 'slim-select'
 import axios from "axios";
 
 axios.defaults.headers.common['X-Requested-With'] = 'XMLHttpRequest';
@@ -7,42 +7,55 @@ let liveSearch = document.querySelectorAll('.live-search');
 for (let i = 0; i < liveSearch.length; i++) {
     let slimSelect = new SlimSelect({
         select: liveSearch[i],
-        searchingText: 'Searching...',
-        allowDeselect: true,
-        placeholder: 'Searching...',
-        ajax: function (search, callback) {
-            // Check search value. If you don't like it then use callback(false) or callback('Message String')
-            if (search.length < 3) {
-                callback('Need 3 characters');
-                return;
-            }
-
-            axios.post(`/${document.documentElement.lang}/ajax/live-search-${liveSearch[i].dataset.entityName}`, {
-                search: search
-            })
-                .then(function (json) {
-                    let data = []
-                    for (let i = 0; i < json.data.length; i++) {
-                        data.push({
-                            value: json.data[i].value,
-                            text: json.data[i].text
-                        })
+        settings: {
+            allowDeselect: true,
+            placeholderText: 'Select value',
+            searchingText: 'Searching...',
+            searchPlaceholder: 'Searching...',
+            searchText: 'No results 😢',
+        },
+        events: {
+            search: function (searchValue, currentData) {
+                return new Promise((resolve, reject) => {
+                    if (searchValue.length < 3) {
+                        return reject('Search value must be at least 3 characters.')
                     }
 
-                    callback(data)
+                    axios.post(`/${document.documentElement.lang}/ajax/live-search-${liveSearch[i].dataset.entityName}`, {
+                        search: searchValue
+                    })
+                        .then(function (json) {
+                            // take the data and create an array of options excluding any that are already selected in currentData
+                            const options = json.data
+                                .filter((user) => {
+                                    return !currentData.some((optionData) => {
+                                        return optionData.value === `${user.value}`
+                                    })
+                                })
+                                .map((user) => {
+                                    return {
+                                        text: `${user.text}`,
+                                        value: `${user.value}`,
+                                    }
+                                })
+
+                            resolve(options)
+                        })
                 })
-                .catch(function (error) {
-                    callback(false)
-                })
+            },
+            error: function (error) {
+                console.log(error)
+            }
         }
     });
 
     if (liveSearch[i].dataset.entityValue && liveSearch[i].dataset.entityText) {
-        let data = [({
-            value: liveSearch[i].dataset.entityValue,
-            text: liveSearch[i].dataset.entityText
-        })];
+        let data = [(
+            {
+                text: liveSearch[i].dataset.entityText,
+                value: liveSearch[i].dataset.entityValue
+            }
+        )];
         slimSelect.setData(data);
-        slimSelect.set([liveSearch[i].dataset.entityValue])
     }
 }

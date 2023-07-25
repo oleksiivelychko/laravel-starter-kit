c81c5f95ae6f4572f45625412ce2be33af15192c
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-07-25 22:49:37 +0300

    apply phpcsfixer ruleset

--- app/Rules/CreditCard.php
+++ app/Rules/CreditCard.php
@@ -4,7 +4,6 @@ namespace App\Rules;
 
 use Illuminate\Contracts\Validation\Rule;
 
-
 class CreditCard implements Rule
 {
     /**
@@ -30,19 +29,23 @@ class CreditCard implements Rule
     /**
      * Luhn algorithm number checker.
      * https://gist.github.com/troelskn/1287893
-     * https://en.wikipedia.org/wiki/Luhn_algorithm
+     * https://en.wikipedia.org/wiki/Luhn_algorithm.
      */
     private function isValidLuhn($number): bool
     {
         settype($number, 'string');
-        $sumTable = array(
-            array(0,1,2,3,4,5,6,7,8,9),
-            array(0,2,4,6,8,1,3,5,7,9));
+
+        $sumTable = [
+            [0, 1, 2, 3, 4, 5, 6, 7, 8, 9],
+            [0, 2, 4, 6, 8, 1, 3, 5, 7, 9]];
+
         $sum = 0;
         $flip = 0;
-        for ($i = strlen($number) - 1; $i >= 0; $i--) {
+
+        for ($i = strlen($number) - 1; $i >= 0; --$i) {
             $sum += $sumTable[$flip++ & 0x1][$number[$i]];
         }
-        return $sum % 10 === 0;
+
+        return 0 === $sum % 10;
     }
 }

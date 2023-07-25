a2a2a7fe4b334f3c7d20cfc3dbf4b83cc0f0d0c7
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-07-25 20:55:35 +0300

    apply phpcsfixer ruleset

--- tests/Feature/API/CategoryCRUDTest.php
+++ tests/Feature/API/CategoryCRUDTest.php
@@ -7,18 +7,22 @@ use App\Models\Category;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class CategoryCRUDTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_create_category()
+    public function testCreateCategory()
     {
         $response = $this->json('POST', '/api/v1/categories', [
-            'name__en'  => 'Test category',
-            'name__uk'  => 'Тестова категорiя'
+            'name__en' => 'Test category',
+            'name__uk' => 'Тестова категорiя',
         ], [
-            'X-API-KEY' => env('API_TOKEN')
+            'X-API-KEY' => env('API_TOKEN'),
         ]);
 
         $content = json_decode($response->content(), true);
@@ -26,23 +30,24 @@ class CategoryCRUDTest extends TestCase
         $response
             ->assertStatus(200)
             ->assertJsonStructure(['parent_id', 'slug', 'name', 'updated_at', 'created_at'])
-            ->assertExactJson($content);
+            ->assertExactJson($content)
+        ;
 
         $this->assertEquals('test-category', $content['slug']);
         $this->assertEquals('Test category', LocaleHelper::translateObject($content['name'], 'en'));
         $this->assertEquals('Тестова категорiя', LocaleHelper::translateObject($content['name'], 'uk'));
     }
 
-    public function test_update_category()
+    public function testUpdateCategory()
     {
         /** @var Category $category */
         $category = Category::factory()->create();
 
         $response = $this->json('PUT', '/api/v1/categories/'.$category->id, [
-            'name__en'  => 'Test '.$category->name,
-            'name__uk'  => 'Тестова '.$category->name
+            'name__en' => 'Test '.$category->name,
+            'name__uk' => 'Тестова '.$category->name,
         ], [
-            'X-API-KEY' => env('API_TOKEN')
+            'X-API-KEY' => env('API_TOKEN'),
         ]);
 
         $content = json_decode($response->content(), true);
@@ -50,53 +55,57 @@ class CategoryCRUDTest extends TestCase
         $response
             ->assertStatus(200)
             ->assertJsonStructure(['parent_id', 'slug', 'name', 'updated_at', 'created_at'])
-            ->assertExactJson($content);
+            ->assertExactJson($content)
+        ;
 
         $this->assertEquals('Test '.$category->name, LocaleHelper::translateObject($content['name'], 'en'));
         $this->assertEquals('Тестова '.$category->name, LocaleHelper::translateObject($content['name'], 'uk'));
     }
 
-    public function test_delete_category()
+    public function testDeleteCategory()
     {
         /** @var Category $category */
         $category = Category::factory()->create();
 
         $response = $this->json('DELETE', '/api/v1/categories/'.$category->id, [], [
-            'X-API-KEY' => env('API_TOKEN')
+            'X-API-KEY' => env('API_TOKEN'),
         ]);
 
         $response
             ->assertStatus(200)
-            ->assertExactJson(['Category has been successfully deleted']);
+            ->assertExactJson(['Category has been successfully deleted'])
+        ;
     }
 
-    public function test_get_category()
+    public function testGetCategory()
     {
         /** @var Category $category */
         $category = Category::factory()->create();
 
         $response = $this->json('GET', '/api/v1/categories/'.$category->id, [], [
-            'X-API-KEY' => env('API_TOKEN')
+            'X-API-KEY' => env('API_TOKEN'),
         ]);
 
         $response
             ->assertStatus(200)
             ->assertJsonStructure(['parent_id', 'slug', 'name', 'updated_at', 'created_at'])
-            ->assertExactJson(json_decode($response->content(), true));
+            ->assertExactJson(json_decode($response->content(), true))
+        ;
     }
 
-    public function test_get_categories()
+    public function testGetCategories()
     {
         Category::factory()->create();
         Category::factory()->create();
 
         $response = $this->json('GET', '/api/v1/categories/', [], [
-            'X-API-KEY' => env('API_TOKEN')
+            'X-API-KEY' => env('API_TOKEN'),
         ]);
 
         $response
             ->assertStatus(200)
             ->assertExactJson(json_decode($response->content(), true))
-            ->assertJsonCount(2);
+            ->assertJsonCount(2)
+        ;
     }
 }
--- tests/Feature/API/ProductCRUDTest.php
+++ tests/Feature/API/ProductCRUDTest.php
@@ -10,12 +10,16 @@ use Illuminate\Http\UploadedFile;
 use Illuminate\Support\Facades\Storage;
 use Tests\TestCase;
 
-
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class ProductCRUDTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_create_product()
+    public function testCreateProduct()
     {
         Storage::fake('uploads');
         $file = UploadedFile::fake()->image('product.jpeg');
@@ -24,15 +28,15 @@ class ProductCRUDTest extends TestCase
         $category = Category::factory()->create();
 
         $response = $this->json('POST', '/api/v1/products', [
-            'name__en'          => 'Test product',
-            'name__uk'          => 'Тестовий продукт',
-            'price'             => 1.2,
-            'description__en'   => 'Test product description',
-            'description__uk'   => 'Тестовий продукт опис',
-            'categories'        => [$category->id],
-            'images'            => [$file]
+            'name__en' => 'Test product',
+            'name__uk' => 'Тестовий продукт',
+            'price' => 1.2,
+            'description__en' => 'Test product description',
+            'description__uk' => 'Тестовий продукт опис',
+            'categories' => [$category->id],
+            'images' => [$file],
         ], [
-            'X-API-KEY' => env('API_TOKEN')
+            'X-API-KEY' => env('API_TOKEN'),
         ]);
 
         $content = json_decode($response->content(), true);
@@ -40,7 +44,8 @@ class ProductCRUDTest extends TestCase
         $response
             ->assertStatus(200)
             ->assertJsonStructure(['description', 'slug', 'name', 'updated_at', 'created_at', 'categories_ids', 'images'])
-            ->assertExactJson($content);
+            ->assertExactJson($content)
+        ;
 
         $this->assertEquals('test-product', $content['slug']);
         $this->assertEquals('Test product', LocaleHelper::translateObject($content['name'], 'en'));
@@ -51,7 +56,7 @@ class ProductCRUDTest extends TestCase
         Storage::disk('uploads')->assertExists('products_images/'.$content['id'].'/'.json_decode($content['images'], true)[0]);
     }
 
-    public function test_update_product()
+    public function testUpdateProduct()
     {
         Storage::fake('uploads');
         $file1 = UploadedFile::fake()->image('product1.jpeg');
@@ -62,19 +67,20 @@ class ProductCRUDTest extends TestCase
 
         /** @var Category $category1 */
         $category1 = Category::factory()->create();
+
         /** @var Category $category2 */
         $category2 = Category::factory()->create();
 
         $response = $this->json('PUT', '/api/v1/products/'.$product->id, [
-            'name__en'          => 'Test '.LocaleHelper::translateObject($product->name, 'en'),
-            'name__uk'          => 'Тестовий '.LocaleHelper::translateObject($product->name, 'uk'),
-            'price'             => 1.1,
-            'description__en'   => 'Test '.LocaleHelper::translateObject($product->description, 'en'),
-            'description__uk'   => 'Тестовий '.LocaleHelper::translateObject($product->description, 'en'),
-            'categories'        => [$category1->id, $category2->id],
-            'images'            => [$file1, $file2]
+            'name__en' => 'Test '.LocaleHelper::translateObject($product->name, 'en'),
+            'name__uk' => 'Тестовий '.LocaleHelper::translateObject($product->name, 'uk'),
+            'price' => 1.1,
+            'description__en' => 'Test '.LocaleHelper::translateObject($product->description, 'en'),
+            'description__uk' => 'Тестовий '.LocaleHelper::translateObject($product->description, 'en'),
+            'categories' => [$category1->id, $category2->id],
+            'images' => [$file1, $file2],
         ], [
-            'X-API-KEY' => env('API_TOKEN')
+            'X-API-KEY' => env('API_TOKEN'),
         ]);
 
         $content = json_decode($response->content(), true);
@@ -82,7 +88,8 @@ class ProductCRUDTest extends TestCase
         $response
             ->assertStatus(200)
             ->assertJsonStructure(['description', 'slug', 'name', 'updated_at', 'created_at', 'categories_ids', 'images'])
-            ->assertExactJson($content);
+            ->assertExactJson($content)
+        ;
 
         $this->assertEquals('test-'.$product->slug, $content['slug']);
         $this->assertEquals('Test '.LocaleHelper::translateObject($product->name, 'en'), LocaleHelper::translateObject($content['name'], 'en'));
@@ -92,53 +99,59 @@ class ProductCRUDTest extends TestCase
         $this->assertContains($category1->id, $content['categories_ids']);
         $this->assertContains($category2->id, $content['categories_ids']);
 
-        $images = array_filter(array_map(function ($image) {return !str_starts_with($image, '/tmp/') ? $image : false;},
-            json_decode($content['images'], true)), function ($image) {return $image;});
+        $images = array_filter(array_map(
+            function ($image) {return !str_starts_with($image, '/tmp/') ? $image : false; },
+            json_decode($content['images'], true)
+        ), function ($image) {return $image; });
+
         Storage::disk('uploads')->assertExists('products_images/'.$content['id'].'/'.array_shift($images));
         Storage::disk('uploads')->assertExists('products_images/'.$content['id'].'/'.array_shift($images));
     }
 
-    public function test_delete_product()
+    public function testDeleteProduct()
     {
         /** @var Product $product */
         $product = Product::factory()->create();
 
         $response = $this->json('DELETE', '/api/v1/products/'.$product->id, [], [
-            'X-API-KEY' => env('API_TOKEN')
+            'X-API-KEY' => env('API_TOKEN'),
         ]);
 
         $response
             ->assertStatus(200)
-            ->assertExactJson(['Product has been successfully deleted']);
+            ->assertExactJson(['Product has been successfully deleted'])
+        ;
     }
 
-    public function test_get_product()
+    public function testGetProduct()
     {
         /** @var Product $product */
         $product = Product::factory()->create();
 
         $response = $this->json('GET', '/api/v1/products/'.$product->id, [], [
-            'X-API-KEY' => env('API_TOKEN')
+            'X-API-KEY' => env('API_TOKEN'),
         ]);
 
         $response
             ->assertStatus(200)
             ->assertJsonStructure(['description', 'slug', 'name', 'updated_at', 'created_at', 'categories_ids', 'images'])
-            ->assertExactJson(json_decode($response->content(), true));
+            ->assertExactJson(json_decode($response->content(), true))
+        ;
     }
 
-    public function test_get_products()
+    public function testGetProducts()
     {
         Product::factory()->create();
         Product::factory()->create();
 
         $response = $this->json('GET', '/api/v1/products/', [], [
-            'X-API-KEY' => env('API_TOKEN')
+            'X-API-KEY' => env('API_TOKEN'),
         ]);
 
         $response
             ->assertStatus(200)
             ->assertExactJson(json_decode($response->content(), true))
-            ->assertJsonCount(2);
+            ->assertJsonCount(2)
+        ;
     }
 }

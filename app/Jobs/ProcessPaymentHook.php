f42d4e77138b5221ac2c44f12418d6981b46a458
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-07-25 23:05:44 +0300

    apply phpcsfixer ruleset

--- app/Jobs/ProcessPaymentHook.php
+++ app/Jobs/ProcessPaymentHook.php
@@ -3,7 +3,6 @@
 namespace App\Jobs;
 
 use App\Models\Order;
-use Exception;
 use Illuminate\Bus\Queueable;
 use Illuminate\Contracts\Queue\ShouldQueue;
 use Illuminate\Foundation\Bus\Dispatchable;
@@ -11,10 +10,12 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-
 class ProcessPaymentHook implements ShouldQueue
 {
-    use Dispatchable, InteractsWithQueue, Queueable, SerializesModels;
+    use Dispatchable;
+    use InteractsWithQueue;
+    use Queueable;
+    use SerializesModels;
 
     private int $orderId;
     private string $orderStatus;
@@ -26,18 +27,19 @@ class ProcessPaymentHook implements ShouldQueue
     }
 
     /**
-     * @throws Exception
+     * @throws \Exception
      */
-    public function handle()
+    public function handle(): void
     {
         $order = Order::find($this->orderId);
+
         if ($order && in_array($this->orderStatus, array_keys(Order::STATUSES))) {
             $order->status = Order::STATUSES[$this->orderStatus];
             $order->save();
 
             Log::channel('hooks')->info(__CLASS__.':orderId:'.$this->orderId);
         } else {
-            throw new Exception('Error processing '.__CLASS__);
+            throw new \Exception('Error processing '.__CLASS__);
         }
     }
 }
--- app/Listeners/LogVerifiedUserListener.php
+++ app/Listeners/LogVerifiedUserListener.php
@@ -6,16 +6,10 @@ use Illuminate\Support\Facades\Log;
 
 class LogVerifiedUserListener
 {
-    /**
-     * Create the event listener.
-     */
     public function __construct()
     {
     }
 
-    /**
-     * Handle the event.
-     */
     public function handle($event): void
     {
         Log::channel('daily')->info(

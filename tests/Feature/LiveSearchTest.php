9d0c6b268f8bfa11f785dacba60f36a100119876
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-07-25 20:16:39 +0300

    apply phpcsfixer ruleset

--- tests/Feature/LiveSearchTest.php
+++ tests/Feature/LiveSearchTest.php
@@ -8,47 +8,53 @@ use App\Models\Product;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Tests\TestCase;
 
-
+/**
+ * @internal
+ *
+ * @coversNothing
+ */
 class LiveSearchTest extends TestCase
 {
     use RefreshDatabase;
 
-    public function test_live_search_categories()
+    public function testSearchCategories()
     {
         /** @var Category $category */
         $category = Category::factory()->create();
 
         $response = $this->json('POST', '/'.app()->getLocale().'/ajax/live-search-category', [
-            'search' => LocaleHelper::translateObject($category->name)
+            'search' => LocaleHelper::translateObject($category->name),
         ], [
-            'HTTP_X-Requested-With' => 'XMLHttpRequest'
+            'HTTP_X-Requested-With' => 'XMLHttpRequest',
         ]);
 
         $response
             ->assertStatus(200)
             ->assertJsonStructure([['value', 'text']])
-            ->assertExactJson(json_decode($response->content(), true));
+            ->assertExactJson(json_decode($response->content(), true))
+        ;
 
         $content = json_decode($response->content(), true)[0];
         $this->assertEquals($category->id, $content['value']);
         $this->assertEquals(LocaleHelper::translateObject($category->name), $content['text']);
     }
 
-    public function test_live_search_products()
+    public function testSearchProducts()
     {
         /** @var Product $product */
         $product = Product::factory()->create();
 
         $response = $this->json('POST', '/'.app()->getLocale().'/ajax/live-search-product', [
-            'search' => LocaleHelper::translateObject($product->name)
+            'search' => LocaleHelper::translateObject($product->name),
         ], [
-            'HTTP_X-Requested-With' => 'XMLHttpRequest'
+            'HTTP_X-Requested-With' => 'XMLHttpRequest',
         ]);
 
         $response
             ->assertStatus(200)
             ->assertJsonStructure([['value', 'text']])
-            ->assertExactJson(json_decode($response->content(), true));
+            ->assertExactJson(json_decode($response->content(), true))
+        ;
 
         $content = json_decode($response->content(), true)[0];
         $this->assertEquals($product->id, $content['value']);

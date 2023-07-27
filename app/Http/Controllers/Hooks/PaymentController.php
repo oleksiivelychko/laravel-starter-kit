05d2cb7a3013f31579c3d7ece05ad845c30de1bd
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-07-27 09:24:51 +0300

    rename job to handler

--- app/Http/Controllers/Hooks/PaymentController.php
+++ app/Http/Controllers/Hooks/PaymentController.php
@@ -3,13 +3,13 @@
 namespace App\Http\Controllers\Hooks;
 
 use App\Http\Controllers\Controller;
-use App\Jobs\ProcessPaymentHook;
+use App\Jobs\PaymentHandler;
 use Illuminate\Support\Facades\Request;
 
 class PaymentController extends Controller
 {
     public function index(): void
     {
-        ProcessPaymentHook::dispatch(Request::post('order_id', 0), Request::post('status'));
+        PaymentHandler::dispatch(Request::post('order_id', 0), Request::post('status'));
     }
 }
--- app/Jobs/PaymentHandler.php
+++ app/Jobs/PaymentHandler.php
@@ -10,7 +10,7 @@ use Illuminate\Queue\InteractsWithQueue;
 use Illuminate\Queue\SerializesModels;
 use Illuminate\Support\Facades\Log;
 
-class ProcessPaymentHook implements ShouldQueue
+class PaymentHandler implements ShouldQueue
 {
     use Dispatchable;
     use InteractsWithQueue;

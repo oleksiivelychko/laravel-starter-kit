e4f1d2d2dc28d917f060245339a1bd593e0a960b
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-07-28 08:13:39 +0300

    apply phpcsfixer ruleset

--- app/Helpers/UrlHelper.php
+++ app/Helpers/UrlHelper.php
@@ -4,14 +4,13 @@ namespace App\Helpers;
 
 use Illuminate\Support\Facades\Request;
 
-
 class UrlHelper
 {
     public static function sortable(string $column): string
     {
         $direction = 'asc';
         if ((Request::exists('sort') && Request::exists('direction'))
-            && (Request::query('sort') === $column && Request::query('direction') === 'asc')) {
+            && (Request::query('sort') === $column && 'asc' === Request::query('direction'))) {
             $direction = 'desc';
         }
 

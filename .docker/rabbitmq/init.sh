18aa77f0eae1b2dc2cf686f6646307081830d516
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-07-29 13:03:05 +0300

    allow to get access to rabbitmq ui

--- .docker/rabbitmq/init.sh
+++ .docker/rabbitmq/init.sh
@@ -1,12 +1,12 @@
 #!/bin/sh
 
-# Create Rabbitmq user
+# Create Rabbitmq user.
 ( rabbitmqctl wait --timeout 60 $RABBITMQ_PID_FILE ; \
 rabbitmqctl add_user $RABBITMQ_USER $RABBITMQ_PASSWORD 2>/dev/null ; \
 rabbitmqctl set_user_tags $RABBITMQ_USER administrator ; \
 rabbitmqctl set_permissions -p / $RABBITMQ_USER  ".*" ".*" ".*" ; \
 echo "*** User '$RABBITMQ_USER' with password '$RABBITMQ_PASSWORD' completed. ***" ; \
-echo "*** Log in the WebUI at port 15672 (example: http:/localhost:15672) ***") &
+echo "*** Log in the WebUI at port 15672 (example: https://mq.laravel-starter-kit.local) ***") &
 
 # $@ is used to pass arguments to the rabbitmq-server command.
 # For example if you use it like this: docker run -d rabbitmq arg1 arg2,

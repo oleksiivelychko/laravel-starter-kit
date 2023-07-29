31819c63b69a1d16e96e9879bc8cfe8f3ae65dfe
Oleksii Velychko <oleksiivelychko@icloud.com>
2023-07-29 11:58:50 +0300

    edit host name

--- routes/console.php
+++ routes/console.php
@@ -18,5 +18,5 @@ Artisan::command('test-mail', function () {
     Mail::send('components/application-version', [], function ($message) {
         $message->to('test@test.test')->subject('test mail');
     });
-    $this->comment('check mailbox at https://mail.laravel-dashboard.local');
-})->purpose('Test sending an email to MailHog server');
+    $this->comment('check mailbox at https://mail.laravel-starter-kit.local');
+})->purpose('Test sending an email to MailHog server.');
